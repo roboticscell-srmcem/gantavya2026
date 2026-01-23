b02d32f50ad6613bd5d9a1289aa3d8a2b62775cb
ISHASHWAT-SINGHI <145699719+ISHASHWAT-SINGHI@users.noreply.github.com>
2026-01-23 20:35:40 +0530

    Update gallery component

--- src/components/blocks/gallery.tsx
+++ src/components/blocks/gallery.tsx
@@ -4,74 +4,74 @@ import React from 'react';
 import Masonry from '@/components/ui/masonry';
 
 function Gallery() {
-    const items = [
-        {
-            id: "1",
-            img: "/images/g-1.jpg",
-            height: 500,
-        },
-        {
-            id: "3",
-            img: "/images/g-3.jpg",
-            height: 400,
-        },
-        {
-            id: "4",
-            img: "/images/g-4.jpg",
-            height: 350,
-        },
-        {
-            id: "5",
-            img: "/images/g-5.jpg",
-            height: 500,
-        },
+  const items = [
+    {
+      id: "1",
+      img: "/images/g-1.jpg",
+      height: 500,
+    },
+    {
+      id: "3",
+      img: "/images/g-3.jpg",
+      height: 400,
+    },
+    {
+      id: "4",
+      img: "/images/g-4.jpg",
+      height: 350,
+    },
+    // {
+    //     id: "5",
+    //     img: "/images/g-5.jpg",
+    //     height: 500,
+    // },
 
-        {
-            id: "8",
-            img: "/images/g-8.jpg",
-            height: 350,
-        },
-        {
-            id: "9",
-            img: "/images/g-9.jpg",
-            height: 450,
-        },
-        {
-            id: "10",
-            img: "/images/g-10.jpg",
-            height: 380,
-        },
-        {
-            id: "11",
-            img: "/images/g-11.jpg",
-            height: 420,
-        },
-        {
-            id: "12",
-            img: "/images/g-12.jpg",
-            height: 360,
-        },
-        {
-            id: "13",
-            img: "/images/g-13.jpg",
-            height: 520,
-        },
-        {
-            id: "14",
-            img: "/images/g-14.jpg",
-            height: 390,
-        },
-        {
-            id: "15",
-            img: "/images/g-15.jpeg",
-            height: 450,
-        },
-        {
-            id: "16",
-            img: "/images/g-16.jpg",
-            height: 420,
-        },
-    ];
+    // {
+    //   id: "8",
+    //   img: "/images/g-8.jpg",
+    //   height: 350,
+    // },
+    {
+      id: "9",
+      img: "/images/g-9.jpg",
+      height: 450,
+    },
+    {
+      id: "10",
+      img: "/images/g-10.jpg",
+      height: 380,
+    },
+    {
+      id: "11",
+      img: "/images/g-11.jpg",
+      height: 420,
+    },
+    {
+      id: "12",
+      img: "/images/g-12.jpg",
+      height: 360,
+    },
+    {
+      id: "13",
+      img: "/images/g-13.jpg",
+      height: 520,
+    },
+    {
+      id: "14",
+      img: "/images/g-14.jpg",
+      height: 390,
+    },
+    {
+      id: "15",
+      img: "/images/g-15.jpeg",
+      height: 450,
+    },
+    {
+      id: "16",
+      img: "/images/g-16.jpg",
+      height: 420,
+    },
+  ];
 
   return (
     <section id="gallery" className="relative w-full md:min-h-screen bg-black py-16 sm:py-20 md:py-24 lg:py-32 overflow-hidden">
